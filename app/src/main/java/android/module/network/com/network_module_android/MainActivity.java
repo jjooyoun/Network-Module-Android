0fd4b3153f563fbaa772fb3b3b9dc9621a8a6328
jjooyoun <jjooyoun@gmail.com>
2015-12-29 13:28:38 -0800

    Network-Module-Init
    Network-Module-Init

---
+++ app/src/main/java/android/module/network/com/network_module_android/MainActivity.java
@@ -0,0 +1,13 @@
+package android.module.network.com.network_module_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
